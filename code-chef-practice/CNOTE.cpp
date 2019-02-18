12c9116501702c443fb6e350a1a511046b6b7c6c
sree2chin <sree2chin@gmail.com>
2019-02-19 00:22:16 +0530

    add CNOTE

---
+++ code-chef-practice/CNOTE.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+string ifPossible(int pagesNeeded, int money, int pages[], int costs[], int N) {
+    int i;
+    for(i=0; i<N; i++) {
+        if(pages[i] >= pagesNeeded && costs[i] <= money) {
+            return "LuckyChef";
+        }
+    }   
+    return "UnluckyChef";
+}
+
+int main() {
+    int noOfTests;
+    cin >> noOfTests;
+    while(noOfTests > 0) {
+        int X,Y,K,N;
+    
+        cin >> X; // # of Pages to write
+        cin >> Y; // # of pages left
+        cin >> K; //  money left
+        cin >> N; // # of books
+        
+        int P[N];
+        int C[N];
+        int i;
+        for (i = 0; i < N; ++i)
+        {
+            cin >> P[i];
+            cin >> C[i];
+            
+        }
+        cout << ifPossible(X-Y, K, P, C, N) << endl;
+        noOfTests--;
+    } 
+    return 0;
+}
+
